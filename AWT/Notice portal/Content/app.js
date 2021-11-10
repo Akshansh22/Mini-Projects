25ba2beda82712489c67c34225709084b66fe08c
Akshansh <s.akshansh01@gmail.com>
2021-11-10 23:44:58 +0530

    changes

--- AWT/Notice portal/Content/app.js
+++ AWT/Notice portal/Content/app.js
@@ -1,7 +1,7 @@
 const menu = [
   {
     id: 1,
-    title: "buttermilk pancakes",
+    title: "End Sem Exams",
     category: "CT",
     price: 15.99,
     img: "./images/item-1.jpeg",
@@ -151,3 +151,17 @@ function displayMenuButtons() {
     });
   });
 }
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+  // console.log(links.classList);
+  // console.log(links.classList.contains("random"));
+  // console.log(links.classList.contains("links"));
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links");
+  // } else {
+  //   links.classList.add("show-links");
+  // }
+  links.classList.toggle("show-links");
+});
